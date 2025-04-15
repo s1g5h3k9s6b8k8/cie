f1f34930b4fb05248e58da068b9928e3d2b1dcb2
sanjanaitigatti <sanjanaitigatti@email.com>
2025-04-15 10:47:21 +0530

    Saved

---
+++ VotingEligibility.java
@@ -0,0 +1,20 @@
+package CIE;
+
+import java.util.Scanner;
+
+public class VotingEligibility {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your age: ");
+        int age = scanner.nextInt();
+
+        if (age >= 18) {
+            System.out.println("You are eligible to vote!");
+        } else {
+            System.out.println("You are not eligible to vote. You must be at least 18 years old.");
+        }
+
+        scanner.close();
+    }
+}
